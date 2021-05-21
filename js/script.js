479275cff6e01a1828929fe875cba72a722d622a
herrkingsley <jonas.carboo@gmail.com>
2021-05-21 13:35:30 +0200

    Header och mobil meny färdigt. Påbörjat Hero

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+
+//--------------- Mobil meny ------------------
+
+function openSiteMenu() {
+    document.querySelector('.site-nav').classList.toggle("toggleMenu");
+
+}
+
+
+//--------------- end Mobil meny ------------------
+
 let renderProducts = [];
 let loopproducts = products.produkter.forEach(element => {
     renderProducts += `${element.name} <br> ${element.price} <br><br>`;;
