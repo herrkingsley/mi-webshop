1a94f5ab03044f377a538f78e04ffda4c5319f77
herrkingsley <jonas.carboo@gmail.com>
2021-05-21 14:43:42 +0200

    Slideshow script färdigt, däremot är inte navigeringspilarna kopplade. Gör om jag hinner med.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ function openSiteMenu() {
 
 //--------------- end Mobil meny ------------------
 
+
+
 let renderProducts = [];
 let loopproducts = products.produkter.forEach(element => {
     renderProducts += `${element.name} <br> ${element.price} <br><br>`;;
@@ -16,3 +18,30 @@ let loopproducts = products.produkter.forEach(element => {
 document.getElementById('javatest').innerHTML = `<p>${renderProducts}</p>`;
 
 
+//--------------- Hero Slideshow ------------------
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//   showSlides(slideIndex = n);
+// }
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("hero-slide");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; // hides all the slides
+    }
+    
+    slideIndex++; 
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block"; // Shows the current slide
+    setTimeout(showSlides, 7000); // Change slide every X seconds
+  }
