3ba07f62ee4c1d37767107909be015f5f3805509
herrkingsley <jonas.carboo@gmail.com>
2021-05-21 08:20:03 +0200

    Styleat site meny, påbörjar mobilmeny

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ let renderProducts = [];
 let loopproducts = products.produkter.forEach(element => {
     renderProducts += `${element.name} <br> ${element.price} <br><br>`;;
 });
-document.getElementById('javatest').innerHTML = renderProducts;
+document.getElementById('javatest').innerHTML = `<p>${renderProducts}</p>`;
 
 
