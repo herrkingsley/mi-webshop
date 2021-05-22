84574d616de79a864c32978b39cc6b49eb0e8be5
herrkingsley <jonas.carboo@gmail.com>
2021-05-22 10:06:05 +0200

    Testade att ha dynamisk content via AJAX, skapade en icludeHTML funktion som lägger in html där man vill

---
+++ js/ajax.js
@@ -0,0 +1,13 @@
+function includeHTML(ID, file) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById(`${ID}`).innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", `../${file}.html`, true);
+    xhttp.send();
+  }
+
+ 
